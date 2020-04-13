a68dc1a95245f7880f9869bde891419eab7b8398
Alberto Rodriguez <arodu.dev@gmail.com>
2020-04-12 20:25:49 -0400

    Updates

---
+++ precache-manifest.07775c30a87b6834c3c7e1c636ce7380.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c713bbcef3e353b489c10ed41761ad",
+    "url": "/rmplayer/index.html"
+  },
+  {
+    "revision": "cbe09d385f57b9081087",
+    "url": "/rmplayer/static/css/main.71ab1c50.chunk.css"
+  },
+  {
+    "revision": "271c0ca6d35e707ef229",
+    "url": "/rmplayer/static/js/2.72883841.chunk.js"
+  },
+  {
+    "revision": "cbe09d385f57b9081087",
+    "url": "/rmplayer/static/js/main.bb21ea26.chunk.js"
+  },
+  {
+    "revision": "034f187019836553467b",
+    "url": "/rmplayer/static/js/runtime~main.7e81bac5.js"
+  }
+]);
